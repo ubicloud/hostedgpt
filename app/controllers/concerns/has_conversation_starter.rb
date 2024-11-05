5d6541380e673979e60365f0df1b659d7223953e
Benjamin Satzger <ben@ubicloud.com>
2024-11-05 18:29:47 +0100

    Update conversation starters

--- app/controllers/concerns/has_conversation_starter.rb
+++ app/controllers/concerns/has_conversation_starter.rb
@@ -6,18 +6,18 @@ module HasConversationStarter
   def set_conversation_starters
     conversation_starters = [
       ["Suggest a healthy meal plan", "for a week for someone with a busy schedule"],
-      ["Plan a solo travel itinerary", "for a weekend getaway in a mountainous region"],
+      ["What are the top startup conferences", "to attend in Europe"],
       ["Compose a short biography of Matz", "focusing on his philosophy behind creating Ruby"],
-      ["Help me pick", "an outfit that will look good on camera"],
+      ["Help me pick", "a business outfit that will look good on camera"],
       ["Create a personal webpage for me", "after asking me three questions"],
       ["Recommend activities", "for a team-building day with remote employees"],
       ["Write a SQL query", "that adds a \"status\" column to an \"orders\" table"],
       ["Explain the significance of quantum computing", "in addressing complex problems"],
-      ["Create a weekly newsletter template", "for a gardening club"],
+      ["Create a weekly newsletter template", "for a long-running project"],
       ["Recommend traditional Bavarian dishes", "and where to find them in Munich"],
       ["Craft a clever invitation", "for an engineering book club"],
       ["Write a survival guide", "for making it through a day with no coffee"],
-      ["Help me plan", "a roadtrip from Charleston to Key West"]
+      ["Help me plan", "a roadtrip from Amsterdam to Paris"]
     ]
 
     @conversation_starters = conversation_starters.shuffle[0..3]
